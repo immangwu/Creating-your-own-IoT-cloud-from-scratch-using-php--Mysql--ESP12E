af2d2309bea284a836764dfc741d771b0e03f3cf
IMMANUAL R <75303021+immangwu@users.noreply.github.com>
2022-09-24 13:26:03 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -5,9 +5,10 @@
 /***************     www.iotmonk.com     *************/
 /****************************************************/ 
 
-define('DB_USER', "phoenix");     // Your database user name
+define('DB_USER', "id19593040_phoeniximman");     // Your database user name
 define('DB_PASSWORD', "@JESUS143su@");			// Your database password (mention your db password here)
-define('DB_DATABASE', "phoeniximman"); // Your database name
+define('DB_DATABASE', "id19593040_phoenix"); // Your database name
 define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
 
 ?>
+
